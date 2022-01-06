1dec8dbc652929ab3e7ddf94f7f9fe365a715b63
iSpookyLemon <77311956+iSpookyLemon@users.noreply.github.com>
2022-01-06 16:42:01 -0600

    Sage and Laboratory

---
+++ src/newplayer/Sage.java
@@ -0,0 +1,11 @@
+package newplayer;
+import battlecode.common.*;
+
+public class Sage extends RobotPlayer {
+    Sage() throws GameActionException {
+
+    }
+    void runSage() throws GameActionException {
+
+    }
+}
